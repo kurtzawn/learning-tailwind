46286e22c7961bc78f02cdca1dbf8fa6220614b1
kurtzawn <kurtz.awn@gmail.com>
2022-06-13 14:38:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './src/**/*.{html,js}',
+      './dist/index.html',
+    ],
+  theme: {
+    container:{
+      center:true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
